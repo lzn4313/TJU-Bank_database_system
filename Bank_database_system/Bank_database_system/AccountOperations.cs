be7687816ebe501219f4f68f1341258dfe585158
Heinz <aurelianob808@gmail.com>
2024-07-11 20:32:13 +0800

    账户操作中的查找银行卡 、冻结、解冻、销户

--- Bank_database_system/Bank_database_system/AccountOperations.cs
+++ Bank_database_system/Bank_database_system/AccountOperations.cs
@@ -4,18 +4,154 @@ using System.Data;
 namespace Bank_database_system
 {
     /*
-     * Account_Operations类中包含账户操作的函数，函数中的所有的MessageBox显示信息都是暂时调试用的
+     * AccountOperations类中包含账户操作的函数，函数中的所有的MessageBox显示信息都是暂时调试用的
      */
-    public static class Account_Operations
+    public static class AccountOperations
     {
+        // 进行账户操作时的操作数
+        public enum OperateNumber
+        {
+            Freeze, // 冻结
+            Unfreeze, // 解冻
+            accountClosure // 销户
+        };
+
         public const string connectionString =
                 @"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=47.96.39.153)(PORT=1521))(CONNECT_DATA=(SID=orcl)));
                 User Id=system;Password=Tongji123456;";
 
-        // 显示用户查询的结果
-        public static void Customer_inquiry_result(string ID)
+        // 显示银行卡查询的结果
+        public static void BankCardInquiryResult(string bankCardID)
         {
-            DataTable dataTable = Customer_inquiry(ID);
+            DataTable accountTable = BankCardInquiry(bankCardID);
+
+            // 处理查询结果
+            if (accountTable.Rows.Count == 0)
+            {
+                MessageBox.Show("查询失败");
+            }
+            // 获取其他字段的值并处理
+            foreach (DataRow row in accountTable.Rows)
+            {
+                MessageBox.Show($"Credit Card Number: {row["CREDIT_CARD_NUMBERS"]}\n" +
+                    $" Identification Number: {row["IDENTIFICATION_NUMBER"]}");
+            }
+        }
+
+        // 银行卡查询的步骤
+        private static DataTable BankCardInquiry(string bankCardID)
+        {
+            // 返回的表
+            DataTable dataTable = new DataTable();
+
+            // 创建数据库连接对象
+            try
+            {
+                using (OracleConnection connection = new(connectionString))
+                {
+                    // 打开数据库连接
+                    connection.Open();
+
+                    // 数据库操作
+                    string selectQuery = "SELECT * FROM ACCOUNT WHERE CREDIT_CARD_NUMBERS = :bankCardID";
+                    using (OracleCommand command = new OracleCommand(selectQuery, connection))
+                    {
+                        OracleParameter param = new OracleParameter(":bankCardID", OracleDbType.Varchar2);
+                        param.Value = bankCardID; // 替换成实际的银行卡号
+                        command.Parameters.Add(param);
+
+                        // 执行查询
+                        using (OracleDataReader reader = command.ExecuteReader())
+                        {
+                            dataTable.Load(reader); // 将查询结果加载到 DataTable
+                            return dataTable;
+                        }
+                    }
+                }
+            }
+            catch (OracleException ex)
+            {
+                // 处理 Oracle 数据库相关异常
+                MessageBox.Show($"数据库操作失败：{ex.Message}", "数据库错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new DataTable();
+            }
+            catch (Exception ex)
+            {
+                // 处理其他类型的异常
+                MessageBox.Show($"发生错误：{ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new DataTable();
+            }
+        }
+
+        // 冻结、解冻、销户
+        public static bool BankCardOperate(string bankCardID, OperateNumber operate)
+        {
+            try
+            {
+                using (OracleConnection connection = new(connectionString))
+                {
+                    // 打开数据库连接
+                    connection.Open();
+                    string operateQuery = "";
+                    string state = "";
+                    switch (operate)
+                    {
+                        case OperateNumber.Freeze:
+                            operateQuery = @"UPDATE ACCOUNT SET STATE = :state 
+                                            WHERE CREDIT_CARD_NUMBERS = :bankCardID";
+                            state = "冻结";
+                            break;
+                        case OperateNumber.Unfreeze:
+                            operateQuery = @"UPDATE ACCOUNT SET STATE = :state 
+                                            WHERE CREDIT_CARD_NUMBERS = :bankCardID";
+                            state = "正常";
+                            break;
+                        case OperateNumber.accountClosure:
+                            operateQuery = @"DELETE FROM ACCOUNT
+                                            WHERE CREDIT_CARD_NUMBERS = :bankCardID";
+                            break;
+                    }
+
+                    // 构建更新语句
+                    using (OracleCommand command = new OracleCommand(operateQuery, connection))
+                    {
+                        // 添加参数
+                        if (state != "")
+                        {
+                            command.Parameters.Add(":state", OracleDbType.Varchar2).Value = state;
+                        }
+                        command.Parameters.Add(":bankCardID", OracleDbType.Varchar2).Value = bankCardID;
+
+                        // 执行更新操作
+                        if (command.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (OracleException ex)
+            {
+                // 处理 Oracle 数据库相关异常
+                MessageBox.Show($"数据库操作失败：{ex.Message}", "数据库错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                // 处理其他类型的异常
+                MessageBox.Show($"发生错误：{ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        // 显示用户查询的结果
+        public static void CustomerInquiryResult(string ID)
+        {
+            DataTable dataTable = CustomerInquiry(ID);
             if (dataTable.Rows.Count == 0)
             {
                 MessageBox.Show("查询失败");
@@ -31,7 +167,7 @@ namespace Bank_database_system
         }
 
         // 用户查询的步骤
-        private static DataTable Customer_inquiry(string ID)
+        private static DataTable CustomerInquiry(string ID)
         {
             // 返回的表
             DataTable dataTable = new DataTable();
@@ -75,7 +211,7 @@ namespace Bank_database_system
         }
 
         // 用户插入
-        public static bool Customer_insert(string ID, string name, string address, string phoneNumber)
+        public static bool CustomerInsert(string ID, string name, string address, string phoneNumber)
         {
             try
             {
@@ -122,7 +258,7 @@ namespace Bank_database_system
         }
 
         // 根据身份证号ID更新用户信息
-        public static bool Customer_update(string IDtoUpdate, string newName, string newAddress, string newPhoneNumber)
+        public static bool CustomerUpdate(string IDtoUpdate, string newName, string newAddress, string newPhoneNumber)
         {
             try
             {
