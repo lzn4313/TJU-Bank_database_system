ac4c03a7eeb7586773bd45f754717025e9876ace
Abandonee993 <153714260+Abandonee993@users.noreply.github.com>
2024-08-25 19:38:14 +0800

    补充了财务部门的三个函数
    补充了财务部门的三个函数

--- Bank_database_system/Bank_database_system/Finance.cs
+++ Bank_database_system/Bank_database_system/Finance.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -40,7 +40,7 @@ namespace Bank_database_system
 
                     using (OracleCommand command = new OracleCommand(query_string, connection))
                     {
-                        command.Parameters.Add(new OracleParameter("branch_ID", string.IsNullOrEmpty(branch_ID) ? (object)DBNull.Value : branch_ID));
+                        command.Parameters.Add(new OracleParameter(":branch_ID", OracleDbType.Varchar2)).Value = branch_ID;
 
                         using (OracleDataReader reader = command.ExecuteReader())
                         {
@@ -60,7 +60,7 @@ namespace Bank_database_system
         }
 
         /********************************************************************* 
-         *以分页方式查询所有支行，便于显示
+         *以分页方式查询所有支行信息，便于显示
          *参数：
          *int PageNumber 当前页码
          *int PageSize   页大小
@@ -119,6 +119,161 @@ namespace Bank_database_system
             return "to_be_finished";
         }
 
+        /********************************************************************* 
+         *以分页方式查询本银行所有流水（存款与取款）
+         *参数：
+         *string branch_ID     本行支行ID
+         *int PageNumber       当前页码
+         *int PageSize         页大小
+         *返回查询结果表
+         **********************************************************************/
+        public static DataTable Selcet_All_Transaction(string branch_ID, int PageNumber, int PageSize)
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+                    int offset = (PageNumber - 1) * PageSize;//得到偏移量
 
+                    string query_string = @"SELECT * FROM (
+                                              SELECT a.*, ROWNUM rnum
+                                              FROM (
+                                                SELECT * 
+                                                FROM TRANSACTION_HISTORY
+                                                WHERE (TRANSACTION_COMMENT = :TYPE_1 
+                                                    OR TRANSACTION_COMMENT = :TYPE_2)
+                                                AND CREDIT_CARD_NUMBERS IN 
+                                                (SELECT CREDIT_CARD_NUMBERS 
+                                                 FROM ACCOUNT
+                                                 WHERE BRANCH_ID = :branch_ID)
+                                              ) a
+                                              WHERE ROWNUM <= :MaxRow
+                                            )
+                                            WHERE rnum >= :MinRow";
+
+                    using (OracleCommand command = new OracleCommand(query_string, connection))
+                    {
+                        command.Parameters.Add(new OracleParameter(":TYPE_1", OracleDbType.Varchar2)).Value = "存款";
+                        command.Parameters.Add(new OracleParameter(":TYPE_2", OracleDbType.Varchar2)).Value = "取款";
+                        command.Parameters.Add(new OracleParameter(":branch_ID", OracleDbType.Varchar2)).Value = branch_ID;
+                        command.Parameters.Add(new OracleParameter("MaxRow", offset + PageSize));
+                        command.Parameters.Add(new OracleParameter("MinRow", offset));
+
+                        using (OracleDataReader reader = command.ExecuteReader())
+                        {
+                            DataTable dataTable = new DataTable();
+                            dataTable.Load(reader);
+                            return dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ErrorMessage: " + ex.Message);
+                return new DataTable(); //出错返回空表
+            }
+        }
+
+        /********************************************************************* 
+         *得到银行某段时间的总支出（取款）
+         *参数：
+         *string branch_ID     本行支行ID
+         *DateTime BeginDate   起始日期
+         *DateTime EndDate     终止日期
+         *返回这段时间的总支出（RMB）
+         **********************************************************************/
+        public static decimal GetExpenses(string branch_ID, DateTime BeginDate, DateTime EndDate)
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query_string = @"SELECT SUM(AMOUNT) 
+                                            FROM TRANSACTION_HISTORY
+                                            WHERE TRANSACTION_TIME >= :BeginDate
+                                            AND TRANSACTION_TIME <= :EndDate
+                                            AND TRANSACTION_COMMENT = :TYPE
+                                            AND CREDIT_CARD_NUMBERS IN 
+                                            (SELECT CREDIT_CARD_NUMBERS 
+                                             FROM ACCOUNT
+                                             WHERE BRANCH_ID = :branch_ID)";
+
+                    using (OracleCommand command = new OracleCommand(query_string, connection))
+                    {
+                        command.Parameters.Add(new OracleParameter(":BeginDate", OracleDbType.Date)).Value = BeginDate;
+                        command.Parameters.Add(new OracleParameter(":EndDate", OracleDbType.Date)).Value = EndDate;
+                        command.Parameters.Add(new OracleParameter(":TYPE", OracleDbType.Varchar2)).Value = "取款";
+                        command.Parameters.Add(new OracleParameter(":branch_ID", OracleDbType.Varchar2)).Value = branch_ID;
+
+                        decimal total = 0;
+                        object result = command.ExecuteScalar();
+                        if(result != DBNull.Value)
+                        {
+                            total = Convert.ToDecimal(result);
+                        }
+                        return total;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ErrorMessage: " + ex.Message);
+                return -1; //出错返回-1
+            }
+        }
+
+        /********************************************************************* 
+         *得到银行某段时间的总收入（存款）
+         *参数：
+         *string branch_ID     本行支行ID
+         *DateTime BeginDate   起始日期
+         *DateTime EndDate     终止日期
+         *返回这段时间的总收入（RMB）
+         **********************************************************************/
+        public static decimal GetIncome(string branch_ID, DateTime BeginDate, DateTime EndDate)
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query_string = @"SELECT SUM(AMOUNT) 
+                                            FROM TRANSACTION_HISTORY
+                                            WHERE TRANSACTION_TIME >= :BeginDate
+                                            AND TRANSACTION_TIME <= :EndDate
+                                            AND TRANSACTION_COMMENT = :TYPE
+                                            AND CREDIT_CARD_NUMBERS IN 
+                                            (SELECT CREDIT_CARD_NUMBERS 
+                                             FROM ACCOUNT
+                                             WHERE BRANCH_ID = :branch_ID)";
+
+                    using (OracleCommand command = new OracleCommand(query_string, connection))
+                    {
+                        command.Parameters.Add(new OracleParameter(":BeginDate", OracleDbType.Date)).Value = BeginDate;
+                        command.Parameters.Add(new OracleParameter(":EndDate", OracleDbType.Date)).Value = EndDate;
+                        command.Parameters.Add(new OracleParameter(":TYPE", OracleDbType.Varchar2)).Value = "存款";
+                        command.Parameters.Add(new OracleParameter(":branch_ID", OracleDbType.Varchar2)).Value = branch_ID;
+
+                        decimal total = 0;
+                        object result = command.ExecuteScalar();
+                        if (result != DBNull.Value)
+                        {
+                            total = Convert.ToDecimal(result);
+                        }
+                        return total;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ErrorMessage: " + ex.Message);
+                return -1; //出错返回-1
+            }
+        }
     }
 }
