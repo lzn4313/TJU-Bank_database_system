741b0ef08b110c1d73ac402a151a50ea8f53dc5d
汐Serene <14129435+xi-serene@user.noreply.gitee.com>
2024-08-26 12:22:36 +0800

    前后端初步整合

--- Bank_database_system/Bank_database_system/obj/Debug/net8.0-windows/Bank_database_system.AssemblyInfo.cs
+++ Bank_database_system/Bank_database_system/obj/Debug/net8.0-windows/Bank_database_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank_database_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8483acf03c2258aa720c20d817df3c5ac754a12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab14faba07a2220601a57722d2f7c96b41296a14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank_database_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank_database_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
